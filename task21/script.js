d2d896b71cc99ec739eaad9d74db062f43c3bef5
alex2wong <821435682@qq.com>
2016-04-25 00:02:46 +0800

    task21

---
+++ task21/script.js
@@ -0,0 +1,16 @@
+/*
+	关于对输入的拆分、加入数组的函数
+*/
+(function(){
+	var inter = function(){};
+	var n = 11;
+})()
+var que = {
+	queue:[21,3,34],
+	spliter: spliter
+}
+
+function spliter(argument) {
+	// body...
+	alert(12);
+}
