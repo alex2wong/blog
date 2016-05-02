78addda5566165395adc0efae6cb33b7e4de99a0
alex2wong <821435682@qq.com>
2016-05-02 23:36:07 +0800

    task21 oop

--- task21/script.js
+++ task21/script.js
@@ -5,6 +5,24 @@
 	var inter = function(){};
 	var n = 11;
 })()
+
+function Queue(divlist){
+	this.queue = [];
+	this.render = function(){
+		var inhtml = "";
+		for (var i = 0; i < this.queue.length; i++) {
+			inhtml += "<span id="+ i +">" + this.queue[i] + "</span>";
+		}
+		divlist.innerHTML = inhtml;
+	}
+}
+
+Queue.prototype.rightPush = function(str){
+	this.queue.push(str);
+	this.render();
+}
+
+
 var que = {
 	queue:[21,3,34],
 	spliter: spliter
